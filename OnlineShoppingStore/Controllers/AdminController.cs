f57a38c014667fbc5644bd2e2d4c4d50f96b3cd6
mete <metekaba96@gmail.com>
2019-11-15 23:59:39 +0300

    Paged View has been added.

--- OnlineShoppingStore/Controllers/AdminController.cs
+++ OnlineShoppingStore/Controllers/AdminController.cs
@@ -15,6 +15,18 @@ namespace OnlineShoppingStore.Controllers
         // GET: Admin
 
         public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+
+        public List<SelectListItem> GetCategory()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            var cat = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecords();
+            foreach (var item in cat)
+            {
+                list.Add(new SelectListItem { Value = item.CategoryId.ToString(), Text = item.CategoryName });
+            }
+
+            return list;
+        }
         public ActionResult Dashboard()
         {
             return View();
@@ -45,27 +57,64 @@ namespace OnlineShoppingStore.Controllers
             return View("UpdateCategory", cd);
 
         }
+        public ActionResult CategoryEdit(int catId)
+        {
+            return View(_unitOfWork.GetRepositoryInstance<Tbl_Category>().GetFirstorDefault(catId));
+        }
+        [HttpPost]
+        public ActionResult CategoryEdit(Tbl_Category tbl)
+        {
+            _unitOfWork.GetRepositoryInstance<Tbl_Category>().Update(tbl);
+            return RedirectToAction("Categories");
+        }
+
         public ActionResult Product()
         {
             return View(_unitOfWork.GetRepositoryInstance<Tbl_Product>().GetProduct());
         }
         public ActionResult ProductEdit(int productId)
         {
+            ViewBag.CategoryList = GetCategory();
             return View(_unitOfWork.GetRepositoryInstance<Tbl_Product>().GetFirstorDefault(productId));
         }
         [HttpPost]
-        public ActionResult ProductEdit(Tbl_Product tbl)
+        public ActionResult ProductEdit(Tbl_Product tbl, HttpPostedFileBase file)
         {
+            string picName = null;
+            string picPath;
+
+            if (file != null)
+            {
+                picName = System.IO.Path.GetFileName(file.FileName);
+                picPath = System.IO.Path.Combine(Server.MapPath("~/ProductImages/"), picName);
+                //file is uploaded
+                file.SaveAs(picPath);
+            }
+            tbl.ProductImage = file!=null?picName: tbl.ProductImage;
+            tbl.ModifiedDate = DateTime.Now;
             _unitOfWork.GetRepositoryInstance<Tbl_Product>().Update(tbl);
             return RedirectToAction("Product");
         }
         public ActionResult ProductAdd()
         {
+            ViewBag.CategoryList = GetCategory();
             return View();
         }
         [HttpPost]
-        public ActionResult ProductAdd(Tbl_Product tbl)
+        public ActionResult ProductAdd(Tbl_Product tbl, HttpPostedFileBase file)
         {
+            string picName=null;
+            string picPath;
+
+            if (file!=null)
+            {
+                picName = System.IO.Path.GetFileName(file.FileName);
+                picPath = System.IO.Path.Combine(Server.MapPath("~/ProductImages/"), picName);
+                //file is uploaded
+                file.SaveAs(picPath);
+            }
+            tbl.ProductImage = picName;
+            tbl.CreatedDate = DateTime.Now;
             _unitOfWork.GetRepositoryInstance<Tbl_Product>().Add(tbl);
             return RedirectToAction("Product");
         }
