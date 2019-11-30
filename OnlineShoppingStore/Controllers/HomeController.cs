68797a9d8ad84235977c52b9f16b616fce7630f6
mete <metekaba96@gmail.com>
2019-11-30 23:14:52 +0300

    Add to cart

--- OnlineShoppingStore/Controllers/HomeController.cs
+++ OnlineShoppingStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using OnlineShoppingStore.Models.Home;
+﻿using OnlineShoppingStore.DAL;
+using OnlineShoppingStore.Models.Home;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace OnlineShoppingStore.Controllers
 {
     public class HomeController : Controller
     {
+        dbOnlineShoppingEntities ctx = new dbOnlineShoppingEntities();
         public ActionResult Index(string search, int? page)
         {
             HomeIndexViewModel model = new HomeIndexViewModel();
@@ -27,6 +29,33 @@ namespace OnlineShoppingStore.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult AddToCart(int productId)
+        {
+            if (Session["cart"]==null)
+            {
+                List<Item> cart = new List<Item>();
+                var product = ctx.Tbl_Product.Find(productId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var product = ctx.Tbl_Product.Find(productId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+                Session["cart"] = cart;
+            }
+            return Redirect("Index");
         }
     }
 }
